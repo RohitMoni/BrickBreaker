78df76068a29885dcef85a06594cfdfed5edba08
Rohit Moni <rohitmdxb@gmail.com>
2015-02-24 21:17:05 -0500

    First Commit

---
+++ Assets/Code/BallScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour
+{
+
+    private int _direction; // towards the centre (0) or away from the centre (1)
+    private Transform _innerRing;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _direction = 1;
+	    _innerRing = GameObject.FindGameObjectWithTag("InnerRing").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
