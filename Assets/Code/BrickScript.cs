04a28fe1500d66c583eb154784ee27b076262d29
Rohit Moni <rohitmdxb@gmail.com>
2015-03-03 21:43:24 -0500

    Brick health

--- Assets/Code/BrickScript.cs
+++ Assets/Code/BrickScript.cs
@@ -9,6 +9,9 @@ namespace Assets.Code
         public int HealthTotal;
         public int CurrentHealth;
 
+        /* Sprites */
+        public Sprite BrickHealth0, BrickHealth1, BrickHealth2, BrickHealth3;
+
         /* References */
         private GameObject _brickDestroyEffect;
 
@@ -22,6 +25,7 @@ namespace Assets.Code
             PointValue = 50;
             HealthTotal = 4;
             CurrentHealth = 4;
+            GetComponent<SpriteRenderer>().sprite = BrickHealth0;
             //GetComponent<BoxCollider2D>().enabled = false;
         }
 
@@ -41,37 +45,54 @@ namespace Assets.Code
             {
                 Debug.Log("Brick Destroyed!");
 
-                // Move and rotate particle effect
-                var position = transform.position;
-                position.z = -4;
-                _brickDestroyEffect.transform.position = position;
+                CurrentHealth--;
 
-                var rotationVel = -collision2D.gameObject.GetComponent<BallScript>().Velocity.normalized;
-                var angle = Mathf.Atan2(rotationVel.y, rotationVel.x)*Mathf.Rad2Deg;
-                angle += 90;
-                var rotation = Quaternion.Euler(0, 0, angle);
-                _brickDestroyEffect.transform.rotation = rotation;
-
-                // Play particle effect
-                _brickDestroyEffect.GetComponent<ParticleSystem>().Emit((int)(transform.parent.localScale.x * 8));
-
-                // Add points
-                _gameManager.AddScore(PointValue);
-
-                // Destroy brick
-                Destroy(gameObject);
-
-                // Check for win condition
-                var remainingBricks = GameObject.FindGameObjectsWithTag("Brick");
-                var won = true;
-                foreach (var brick in remainingBricks)
+                switch (CurrentHealth)
                 {
-                    if (brick != gameObject)
-                        won = false;
-                }
+                    case 3:
+                        GetComponent<SpriteRenderer>().sprite = BrickHealth1;
+                        break;
+                    case 2:
+                        GetComponent<SpriteRenderer>().sprite = BrickHealth2;
+                        break;
+                    case 1:
+                        GetComponent<SpriteRenderer>().sprite = BrickHealth3;
+                        break;
+                    case 0:
+                        // Move and rotate particle effect
+                        var position = transform.position;
+                        position.z = -4;
+                        _brickDestroyEffect.transform.position = position;
 
-                if (won)
-                    _gameManager.SetWinLossState(true);
+                        var rotationVel = -collision2D.gameObject.GetComponent<BallScript>().Velocity.normalized;
+                        var angle = Mathf.Atan2(rotationVel.y, rotationVel.x)*Mathf.Rad2Deg;
+                        angle += 90;
+                        var rotation = Quaternion.Euler(0, 0, angle);
+                        _brickDestroyEffect.transform.rotation = rotation;
+
+                        // Play particle effect
+                        _brickDestroyEffect.GetComponent<ParticleSystem>().Emit((int) (transform.parent.localScale.x*8));
+
+                        // Add points
+                        _gameManager.AddScore(PointValue);
+
+                        // Destroy brick
+                        Destroy(gameObject);
+
+                        // Check for win condition
+                        var remainingBricks = GameObject.FindGameObjectsWithTag("Brick");
+                        var won = true;
+                        foreach (var brick in remainingBricks)
+                        {
+                            if (brick != gameObject)
+                                won = false;
+                        }
+
+                        if (won)
+                            _gameManager.SetWinLossState(true);
+
+                        break;
+                }
             }
         }
     }
